0ca8f9bdcdd1038e0652bb579229c23be627f34e
Juevan <86933408+Juevan@users.noreply.github.com>
2022-11-16 19:34:43 -0300

    Finalização do projeto
    Adicionado cifra de cesar

--- js/script.js
+++ js/script.js
@@ -1,16 +1,85 @@
-// Função para alterar o botão de resultado.
-let btnCrypt = document.querySelector('#btn');
-let optCode = document.querySelector('.optCode');
-btnCrypt.remove();
-let typeCrypt = document.querySelector('#crypt');
-typeCrypt.addEventListener('click', btnCryptClick);
-function btnCryptClick(){
-    optCode.append(btnCrypt);
-    btnCrypt.value = 'Criptografar';
+var divChave = document.querySelector(".divChave");
+var selecaoCod = document.addEventListener("click", function () {
+  var codigo = document.getElementById("codigo").value;
+  if (codigo == "cesar") {
+    divChave.style.display = "block";
+  } else {
+    divChave.style.display = "none";
+  }
+});
+
+var radio = document.querySelectorAll(".radio");
+var botao = document.getElementById("botaoResultado");
+var mensagem = document.getElementById("mensagem");
+var chave = document.getElementById("chave");
+var resultado = document.getElementById("resultado");
+
+botao.addEventListener("click", function (event) {
+  event.preventDefault();
+  var codigo = document.getElementById("codigo").value;
+  if (codigo == "cesar" && radio[0].checked) {
+    var valorMsg = mensagem.value.split("");
+    var valorChave = parseInt(chave.value);
+    resultado.value = codificarCesar(valorMsg, valorChave);
+  } else if (codigo == "cesar" && radio[1].checked) {
+    var valorMsg = mensagem.value.split("");
+    var valorChave = parseInt(chave.value);
+    resultado.value = decodificarCesar(valorMsg, valorChave);
+  } else if (codigo == "base64" && radio[0].checked) {
+    var valorMsg = mensagem.value;
+    resultado.value = btoa(valorMsg);
+  } else {
+    var valorMsg = mensagem.value;
+    resultado.value = atob(valorMsg);
+  }
+});
+
+function codificarCesar(msg, chave) {
+  return msg
+    .map((str) => {
+      var entrada = str.charCodeAt();
+      if (entrada >= 65 && entrada <= 90) {
+        return String.fromCharCode(((entrada - 65 + chave) % 26) + 65);
+      } else if (entrada >= 97 && entrada <= 122) {
+        return String.fromCharCode(((entrada - 97 + chave) % 26) + 97);
+      } else {
+        return str;
+      }
+    })
+    .join("");
 }
-let typeDecrypt = document.querySelector('#decrypt');
-typeDecrypt.addEventListener('click', btnDecryptClick);
-function btnDecryptClick(){
-    optCode.append(btnCrypt);
-    btnCrypt.value = 'Descriptografar';
+
+function decodificarCesar(msg, chave) {
+  return msg
+    .map((str) => {
+      var entrada = str.charCodeAt();
+      if (entrada >= 65 && entrada <= 90) {
+        if (entrada - 65 - chave < 0) {
+          return String.fromCharCode(((entrada - 65 - chave + 26) % 26) + 65);
+        } else {
+          return String.fromCharCode(((entrada - 65 - chave) % 26) + 65);
+        }
+      } else if (entrada >= 97 && entrada <= 122) {
+        if (entrada - 97 - chave < 0) {
+          return String.fromCharCode(((entrada - 97 - chave + 26) % 26) + 97);
+        } else {
+          return String.fromCharCode(((entrada - 97 - chave) % 26) + 97);
+        }
+      } else {
+        return str;
+      }
+    })
+    .join("");
 }
+
+radio[0].addEventListener("click", function () {
+  if (radio[0].checked) {
+    botao.innerHTML =`<button><span>Criptografar</span></button>`;
+  }
+});
+
+radio[1].addEventListener("click", function () {
+  if (radio[1].checked) {
+    botao.innerHTML = `<button><span>Descriptografar</span></button>`;
+  }
+});
